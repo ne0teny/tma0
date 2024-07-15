6a584838e77d92d70e90b91078ed36905d92c0b4
unknown <sailaukhanbeknur@gmail.com>
2024-07-15 16:43:40 +0500

    change mine

--- src/Mine.tsx
+++ src/Mine.tsx
@@ -1,15 +1,16 @@
 import { FunctionComponent } from 'react';
 import styles from './scss/Mine.module.scss';
-import AvatarImage from './img/Avatar.png';
-import Frame122Image from './img/Frame 122.png';
-import IconImage from './img/Icon.svg';
-import SubtractImage from './img/Subtract.svg';
-import ImageКубокImage from './img/image кубок.png';
-import Image7Image from './img/image 7.png';
-import Component9Image from './img/Component 9.png';
-import Frame170Image from './img/Frame 170.svg';
-import Frame172Image from './img/Frame 172.svg';
-import Frame109Image from './img/Frame 109.svg';
+import frame122 from './img/Frame 122.png';
+import iconSvg from './img/Icon.svg';
+import avatarPng from './img/Avatar.png';
+import imageCup from './img/image кубок.png';
+import subtractSvg from './img/Subtract.svg';
+import component9Png from './img/Component 9.png';
+import image7Png from './img/image 7.png';
+import component13Png from './img/Component 13.png';
+import frame170Svg from './img/Frame 170.svg';
+import frame172Svg from './img/Frame 172.svg';
+import frame109Svg from './img/Frame 109.svg';
 import NavigationBar from './Navigation';
 
 
@@ -24,7 +25,7 @@ const MineCard: FunctionComponent = () => {
                 <div className={styles.parent}>
                   <div className={styles.div}>Поинты за час</div>
                   <div className={styles.instanceParent}>
-                    <img className={styles.frameChild} alt="" src={Frame122Image} />
+                    <img className={styles.frameChild} alt="" src={frame122} />
                     <div className={styles.softSkill}>+580 000</div>
                   </div>
                 </div>
@@ -33,10 +34,10 @@ const MineCard: FunctionComponent = () => {
                 <div className={styles.parent}>
                   <div className={styles.group}>
                     <div className={styles.div2}>Подписчики</div>
-                    <img className={styles.icon} alt="" src={IconImage} />
+                    <img className={styles.icon} alt="" src={iconSvg} />
                   </div>
                   <div className={styles.instanceParent}>
-                    <img className={styles.icon1} alt="" src={IconImage} />
+                    <img className={styles.icon1} alt="" src={iconSvg} />
                     <div className={styles.softSkill}>580 000</div>
                   </div>
                 </div>
@@ -46,7 +47,7 @@ const MineCard: FunctionComponent = () => {
           <div className={styles.pointBlockGroup}>
             <div className={styles.pointBlock2}>
               <div className={styles.skufsdff}>SKUFsdfF</div>
-              <img className={styles.icon2} alt="" src={IconImage} />
+              <img className={styles.icon2} alt="" src={iconSvg} />
             </div>
             <div className={styles.level89Parent}>
               <div className={styles.level89}>level 8/9</div>
@@ -60,12 +61,12 @@ const MineCard: FunctionComponent = () => {
         </div>
         <div className={styles.profileBlock}>
           <div className={styles.avatarParent}>
-            <img className={styles.avatarIcon} alt="" src={AvatarImage} />
+            <img className={styles.avatarIcon} alt="" src={avatarPng} />
             <div className={styles.nameAndRunk}>
               <div className={styles.namee}>Namee...</div>
               <div className={styles.meme}>(meme)</div>
             </div>
-            <img className={styles.icon1} alt="" src={IconImage} />
+            <img className={styles.icon1} alt="" src={iconSvg} />
           </div>
           <div className={styles.everyDayBonus}>
             <div className={styles.container}>
@@ -73,7 +74,7 @@ const MineCard: FunctionComponent = () => {
                 <p className={styles.p}>Ежедневный</p>
                 <p className={styles.p}>бонус</p>
               </div>
-              <img className={styles.imageIcon} alt="" src={ImageКубокImage} />
+              <img className={styles.imageIcon} alt="" src={imageCup} />
               <div className={styles.notificationError}>
                 <div className={styles.div5}>1</div>
               </div>
@@ -95,7 +96,7 @@ const MineCard: FunctionComponent = () => {
       <div className={styles.div6}>
         <div className={styles.instanceGroup}>
           <div className={styles.subtractParent}>
-            <img className={styles.subtractIcon} alt="" src={SubtractImage} />
+            <img className={styles.subtractIcon} alt="" src={subtractSvg} />
             <div className={styles.frameGroup}>
               <div className={styles.frameDiv}>
                 <div className={styles.div7}>Дачный гений</div>
@@ -104,7 +105,7 @@ const MineCard: FunctionComponent = () => {
               </div>
               <div className={styles.frameItem} />
               <div className={styles.component9Parent}>
-                <img className={styles.frameChild} alt="" src={Component9Image} />
+                <img className={styles.frameChild} alt="" src={component9Png} />
                 <div className={styles.softSkill}>1000</div>
               </div>
             </div>
@@ -112,13 +113,37 @@ const MineCard: FunctionComponent = () => {
               <div className={styles.lvl100}>lvl 100</div>
               <div className={styles.instanceWrapper}>
                 <div className={styles.image7Wrapper}>
-                  <img className={styles.image7Icon} alt="" src={Image7Image} />
+                  <img className={styles.image7Icon} alt="" src={image7Png} />
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Повторяющиеся блоки */}
+          <div className={styles.subtractParent}>
+            <img className={styles.subtractIcon} alt="" src={subtractSvg} />
+            <div className={styles.frameGroup}>
+              <div className={styles.frameDiv}>
+                <div className={styles.div7}>Дачный гений</div>
+                <div className={styles.level89}>Прибыль в час</div>
+                <div className={styles.level89}>+1000 поинтов</div>
+              </div>
+              <div className={styles.frameItem} />
+              <div className={styles.component9Parent}>
+                <img className={styles.frameChild} alt="" src={component9Png} />
+                <div className={styles.softSkill}>1000</div>
+              </div>
+            </div>
+            <div className={styles.lvl100Parent}>
+              <div className={styles.lvl100}>lvl 100</div>
+              <div className={styles.instanceWrapper}>
+                <div className={styles.image7Wrapper}>
+                  <img className={styles.image7Icon} alt="" src={image7Png} />
                 </div>
               </div>
             </div>
           </div>
           <div className={styles.subtractParent}>
-            <img className={styles.subtractIcon} alt="" src={SubtractImage} />
+            <img className={styles.subtractIcon} alt="" src={subtractSvg} />
             <div className={styles.frameGroup}>
               <div className={styles.frameDiv}>
                 <div className={styles.div7}>Дачный гений</div>
@@ -127,7 +152,7 @@ const MineCard: FunctionComponent = () => {
               </div>
               <div className={styles.frameItem} />
               <div className={styles.component9Parent}>
-                <img className={styles.frameChild} alt="" src={Component9Image} />
+                <img className={styles.frameChild} alt="" src={component9Png} />
                 <div className={styles.softSkill}>1000</div>
               </div>
             </div>
@@ -135,129 +160,79 @@ const MineCard: FunctionComponent = () => {
               <div className={styles.lvl100}>lvl 100</div>
               <div className={styles.instanceWrapper}>
                 <div className={styles.image7Wrapper}>
-                  <img className={styles.image7Icon} alt="" src={Image7Image} />
+                  <img className={styles.image7Icon} alt="" src={image7Png} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.subtractParent}>
+            <img className={styles.subtractIcon} alt="" src={subtractSvg} />
+            <div className={styles.frameGroup}>
+              <div className={styles.frameDiv}>
+                <div className={styles.div7}>Дачный гений</div>
+                <div className={styles.level89}>Прибыль в час</div>
+                <div className={styles.level89}>+1000 поинтов</div>
+              </div>
+              <div className={styles.frameItem} />
+              <div className={styles.component9Parent}>
+                <img className={styles.frameChild} alt="" src={component9Png} />
+                <div className={styles.softSkill}>1000</div>
+              </div>
+            </div>
+            <div className={styles.lvl100Parent}>
+              <div className={styles.lvl100}>lvl 100</div>
+              <div className={styles.instanceWrapper}>
+                <div className={styles.image7Wrapper}>
+                  <img className={styles.image7Icon} alt="" src={image7Png} />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className={styles.subtractParent}>
+            <img className={styles.subtractIcon} alt="" src={subtractSvg} />
+            <div className={styles.frameGroup}>
+              <div className={styles.frameDiv}>
+                <div className={styles.div7}>Дачный гений</div>
+                <div className={styles.level89}>Прибыль в час</div>
+                <div className={styles.level89}>+1000 поинтов</div>
+              </div>
+              <div className={styles.frameItem} />
+              <div className={styles.component9Parent}>
+                <img className={styles.frameChild} alt="" src={component9Png} />
+                <div className={styles.softSkill}>1000</div>
+              </div>
+            </div>
+            <div className={styles.lvl100Parent}>
+              <div className={styles.lvl100}>lvl 100</div>
+              <div className={styles.instanceWrapper}>
+                <div className={styles.image7Wrapper}>
+                  <img className={styles.image7Icon} alt="" src={image7Png} />
                 </div>
               </div>
             </div>
           </div>
         </div>
-        <div className={styles.instanceGroup}>
-          <div className={styles.subtractParent}>
-            <img className={styles.subtractIcon} alt="" src={SubtractImage} />
-            <div className={styles.frameGroup}>
-              <div className={styles.frameDiv}>
-                <div className={styles.div7}>Дачный гений</div>
-                <div className={styles.level89}>Прибыль в час</div>
-                <div className={styles.level89}>+1000 поинтов</div>
-              </div>
-              <div className={styles.frameItem} />
-              <div className={styles.component9Parent}>
-                <img className={styles.frameChild} alt="" src={Component9Image} />
-                <div className={styles.softSkill}>1000</div>
-              </div>
-            </div>
-            <div className={styles.lvl100Parent}>
-              <div className={styles.lvl100}>lvl 100</div>
-              <div className={styles.instanceWrapper}>
-                <div className={styles.image7Wrapper}>
-                  <img className={styles.image7Icon} alt="" src={Image7Image} />
-                </div>
-              </div>
+        <div className={styles.frameParent11}>
+          <div className={styles.component13Wrapper}>
+            <img className={styles.component13Icon} alt="" src={component13Png} />
+          </div>
+          <div className={styles.instanceParent6}>
+            <img className={styles.frameIcon} alt="" src={frame170Svg} />
+            <img className={styles.frameChild10} alt="" src={frame172Svg} />
+            <div className={styles.instanceParent7}>
+              <img className={styles.instanceChild} alt="" src={frame109Svg} />
+              <div className={styles.div55}>6500/7000</div>
             </div>
           </div>
-          <div className={styles.subtractParent}>
-            <img className={styles.subtractIcon} alt="" src={SubtractImage} />
-            <div className={styles.frameGroup}>
-              <div className={styles.frameDiv}>
-                <div className={styles.div7}>Дачный гений</div>
-                <div className={styles.level89}>Прибыль в час</div>
-                <div className={styles.level89}>+1000 поинтов</div>
-              </div>
-              <div className={styles.frameItem} />
-              <div className={styles.component9Parent}>
-                <img className={styles.frameChild} alt="" src={Component9Image} />
-                <div className={styles.softSkill}>1000</div>
-              </div>
-            </div>
-            <div className={styles.lvl100Parent}>
-              <div className={styles.lvl100}>lvl 100</div>
-              <div className={styles.instanceWrapper}>
-                <div className={styles.image7Wrapper}>
-                  <img className={styles.image7Icon} alt="" src={Image7Image} />
-                </div>
-              </div>
-            </div>
-          </div>
+          
         </div>
-        <div className={styles.instanceGroup}>
-          <div className={styles.subtractParent}>
-            <img className={styles.subtractIcon} alt="" src={SubtractImage} />
-            <div className={styles.frameGroup}>
-              <div className={styles.frameDiv}>
-                <div className={styles.div7}>Дачный гений</div>
-                <div className={styles.level89}>Прибыль в час</div>
-                <div className={styles.level89}>+1000 поинтов</div>
-              </div>
-              <div className={styles.frameItem} />
-              <div className={styles.component9Parent}>
-                <img className={styles.frameChild} alt="" src={Component9Image} />
-                <div className={styles.softSkill}>1000</div>
-              </div>
-            </div>
-            <div className={styles.lvl100Parent}>
-              <div className={styles.lvl100}>lvl 100</div>
-              <div className={styles.instanceWrapper}>
-                <div className={styles.image7Wrapper}>
-                  <img className={styles.image7Icon} alt="" src={Image7Image} />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className={styles.subtractParent}>
-            <img className={styles.subtractIcon} alt="" src={SubtractImage} />
-            <div className={styles.frameGroup}>
-              <div className={styles.frameDiv}>
-                <div className={styles.div7}>Дачный гений</div>
-                <div className={styles.level89}>Прибыль в час</div>
-                <div className={styles.level89}>+1000 поинтов</div>
-              </div>
-              <div className={styles.frameItem} />
-              <div className={styles.component9Parent}>
-                <img className={styles.frameChild} alt="" src={Component9Image} />
-                <div className={styles.softSkill}>1000</div>
-              </div>
-            </div>
-            <div className={styles.lvl100Parent}>
-              <div className={styles.lvl100}>lvl 100</div>
-              <div className={styles.instanceWrapper}>
-                <div className={styles.image7Wrapper}>
-                  <img className={styles.image7Icon} alt="" src={Image7Image} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={styles.component13Parent}>
-        <img className={styles.frameChild} alt="" src={Component9Image} />
-        <div className={styles.softSkill}>+1000</div>
-      </div>
-      <div className={styles.image5Wrapper}>
-        <img className={styles.frameChild} alt="" src={Frame170Image} />
-        <div className={styles.div9}>0</div>
-      </div>
-      <div className={styles.instanceDiv}>
-        <img className={styles.frameChild} alt="" src={Frame172Image} />
-        <div className={styles.softSkill}>Твой брат, шаман, 1й сезон</div>
-      </div>
-      <div className={styles.instanceDiv}>
-        <img className={styles.frameChild} alt="" src={Frame109Image} />
-        <div className={styles.softSkill}>0</div>
+        
       </div>
       <div className={styles.navigationContainer}>
         <NavigationBar />
       </div>
     </div>
+    
   );
 };
 
